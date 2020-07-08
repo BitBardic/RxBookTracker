fc5da4ba8dccd66501bdf30054d50fbee0db6227
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-08 23:35:16 +0100

    Instantiating a new observable with the construtor

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
 alert('Hello RxJs');
+import { Observable } from 'rxjs';
+import { allBooks } from './data';
+
+function subscribe(subscriber) {
+  for (let book of allBooks) {
+    subscriber.next(book);
+  }
+}
+
+let allBooksObservable$ = new Observable(subscribe);
+
+// An observable is not executed until an object subscribes to it.
+
+allBooksObservable$.subscribe((book) => console.log(book.title));
