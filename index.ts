33e80a5253ddf7502843daf2bd336175a0d1bdd3
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-09 00:13:18 +0100

    Creating observables from existing Data using 'from'

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ alert('Hello RxJs');
 import { Observable, of, from, fromEvent } from 'rxjs';
 import { allBooks, allReaders } from './data';
 
-let source1$ = of('hello', 10, true, allReaders[0].name);
+let source2$ = from(allBooks);
 
-source1$.subscribe(value => console.log(value));
+source2$.subscribe(book => console.log(book.title));
