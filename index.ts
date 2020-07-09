7d96b26d524c72ebd304afb6d43fa324747ef73e
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-09 10:28:47 +0100

    Observer subscribing with callbacks

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import { of } from 'rxjs';
 
-// subscribing with an Observer
-
-let myObserver = {
-  next: (value) => console.log(`Value produced: ${value}`),
-  error: (err) => console.log(`Error: ${err}`),
-  complete: () => console.log(`All done producing values.`),
-};
+// subscribing with callbacks
 
 let sourceObservable$ = of(1, 3, 5);
-sourceObservable$.subscribe(myObserver);
+
+sourceObservable$.subscribe(
+    value => console.log(`Value produced: ${value}`),
+    err => console.log(`Error: ${err}`),
+    () => console.log(`All done producing values.`)
+);
+
+sourceObservable$.subscribe(
+    value => console.log(`Value produced: ${value}`),
+    err => console.log(`Error: ${err}`),
+);
+
+sourceObservable$.subscribe(
+    value => console.log(`Value produced: ${value}`)
+);
