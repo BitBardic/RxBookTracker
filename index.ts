6247c6d903cc4b7b0a3bc94a0b271733e5535472
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-11 18:45:19 +0100

    Controlling the number of values produced

--- index.ts
+++ index.ts
@@ -1,20 +1,33 @@
 import { fromEvent, interval, Observable, of, throwError } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { mergeMap, filter, tap, catchError } from 'rxjs/operators';
+import { mergeMap, filter, tap, catchError, take, takeUntil } from 'rxjs/operators';
 
 
-ajax('/api/errors/500')
-    .pipe(
-        mergeMap(ajaxResponse => ajaxResponse.response),
-        filter(book => book.publicationYear < 1950),
-        tap(oldBook => console.log(`Title: ${oldBook.title}`)),
-        //catchError(err => of({title: 'Corduroy', author: 'Don Freeman'})),
-        //catchError((err, caught) => caught),
-        //catchError(err => throw `Something bad happened - ${err.message}`),
-        catchError(err => return throwError(err.message))
-    )
-    .subscribe(
-        finalValue => console.log(`VALUE: ${finalValue.title}`),
-        error => console.log(`ERROR: ${error}`)
-    );
+let timesDiv = document.getElementById('times');
+let button = document.getElementById('timerButton');
+
+// let timer$ = interval(1000);
+
+let timer$ = new Observable(subscribe => {
+    let i = 0;
+    let intervalID = setInterval(() => {
+        subscribe.next(i++);
+    }, 1000);
+
+    return () => {
+        console.log(`Executing teardown code.`);
+        clearInterval(intervalID);
+    }
+});
+
+let cancelTimer$ = fromEvent(button, 'click');
+
+timer$.pipe(
+    takeUntil(cancelTimer$)
+)
+.subscribe(
+    value => timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`,
+    null,
+    () => console.log('All done!')
+);
 
