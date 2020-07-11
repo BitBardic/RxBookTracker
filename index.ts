21f2676747b4fdc255c3e18ee20b799d3aab0fca
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-11 18:21:47 +0100

    Handling Errors 1

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import { fromEvent, interval, Observable, of } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { mergeMap, filter, tap } from 'rxjs/operators';
+import { mergeMap, filter, tap, catchError } from 'rxjs/operators';
 
 
 ajax('/api/books')
     .pipe(
         mergeMap(ajaxResponse => ajaxResponse.response),
         filter(book => book.publicationYear < 1950),
-        tap(oldBook => console.log(`Title: ${oldBook.title}`))
+        tap(oldBook => console.log(`Title: ${oldBook.title}`)),
+        catchError(err => of({title: 'Corduroy', author: 'Don Freeman'}))
     )
     .subscribe(
-        finalValue => console.log(finalValue)
+        finalValue => console.log(`VALUE: ${finalValue.title}`),
+        error => console.log(`ERROR: ${error}`)
     );
 
