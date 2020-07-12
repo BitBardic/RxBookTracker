505feab45f665cabf5572b403e8aa972ad92f122
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-12 19:59:12 +0100

    Creating new operators with the observable constructor

--- index.ts
+++ index.ts
@@ -1,33 +1,35 @@
 import { fromEvent, interval, Observable, of, throwError } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { mergeMap, filter, tap, catchError, take, takeUntil } from 'rxjs/operators';
+import { mergeMap, filter, tap, catchError, take, takeUntil, flatMap } from 'rxjs/operators';
 
-
-let timesDiv = document.getElementById('times');
-let button = document.getElementById('timerButton');
-
-// let timer$ = interval(1000);
-
-let timer$ = new Observable(subscribe => {
-    let i = 0;
-    let intervalID = setInterval(() => {
-        subscribe.next(i++);
-    }, 1000);
-
-    return () => {
-        console.log(`Executing teardown code.`);
-        clearInterval(intervalID);
+function grabAndLogClassics(year, log) {
+    return source$ => {
+        return new Observable(subscriber => {
+            return source$.subscribe(
+                book => {
+                    if (book.publicationYear < year) {
+                        subscriber.next(book);
+                        if (log) {
+                            console.log(`Classic: ${book.title}`);
+                        }
+                    }
+                },
+                err => subscriber.error(err),
+                () => subscriber.complete()
+            );
+        });
     }
-});
+}
 
-let cancelTimer$ = fromEvent(button, 'click');
-
-timer$.pipe(
-    takeUntil(cancelTimer$)
-)
-.subscribe(
-    value => timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`,
-    null,
-    () => console.log('All done!')
-);
+ajax('/api/books')
+    .pipe(
+        flatMap(ajaxResponse => ajaxResponse.response),
+        // filter(book => book.publicationYear < 1950),
+        // tap(oldBook => console.log(`Title: ${oldBook}`))
+        grabAndLogClassics(1930, false)
+    )
+    .subscribe(
+        finalValue => console.log(`VALUE: ${finalValue.title}`),
+        error => console.log(`ERROR: ${error}`)
+    );
 
