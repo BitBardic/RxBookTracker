a997564d78a9c3d8a4d2e8b67e192ec0c3622160
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-13 19:42:46 +0100

    Using muticast operators(mulicast + connect) instead of subject

--- index.ts
+++ index.ts
@@ -1,26 +1,30 @@
 import { fromEvent, interval, Observable, of, throwError, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { mergeMap, filter, tap, catchError, take, takeUntil, flatMap } from 'rxjs/operators';
+import { mergeMap, filter, tap, catchError, take, takeUntil, flatMap,
+         multicast, refCount, publish, share } from 'rxjs/operators';
 
 let source$ = interval(1000).pipe(
-    take(4)
+    take(4),
+    multicast(new Subject())
 );
 
-let subject$ = new Subject();
-source$.subscribe(subject$);
+// let subject$ = new Subject();
+// source$.subscribe(subject$);
 
-subject$.subscribe(
+source$.subscribe(
     value => console.log(`Observer 1: ${value}`)
 );
 
 setTimeout(() => {
-    subject$.subscribe(
+    source$.subscribe(
         value => console.log(`Observer 2: ${value}`)
     );
 }, 1000);
 
 setTimeout(() => {
-    subject$.subscribe(
+    source$.subscribe(
         value => console.log(`Observer 3: ${value}`)
     );
 }, 2000);
+
+source$.connect();
