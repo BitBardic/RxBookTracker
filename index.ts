21023efb5afd8873ee512cee0bd30ef7de1f266d
Nasir Iqbal <nasirpucit@hotmail.com>
2020-07-13 19:24:49 +0100

    Using a subject to convert an observable from cold to hot

--- index.ts
+++ index.ts
@@ -2,20 +2,25 @@ import { fromEvent, interval, Observable, of, throwError, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { mergeMap, filter, tap, catchError, take, takeUntil, flatMap } from 'rxjs/operators';
 
+let source$ = interval(1000).pipe(
+    take(4)
+);
+
 let subject$ = new Subject();
+source$.subscribe(subject$);
 
 subject$.subscribe(
     value => console.log(`Observer 1: ${value}`)
 );
 
-subject$.subscribe(
-    value => console.log(`Observer 2: ${value}`)
-);
+setTimeout(() => {
+    subject$.subscribe(
+        value => console.log(`Observer 2: ${value}`)
+    );
+}, 1000);
 
-subject$.next("Hello!");
-
-let source$ = new Observable(subscriber => {
-    subscriber.next('Greetings!');
-});
-
-source$.subscribe(subject$);
+setTimeout(() => {
+    subject$.subscribe(
+        value => console.log(`Observer 3: ${value}`)
+    );
+}, 2000);
